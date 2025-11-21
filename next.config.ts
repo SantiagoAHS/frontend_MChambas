3471b3a347bbc05ee2002a6d83289bd15ce88da9
SantiagoAHS <lionelsanti2018@gmail.com>
2025-11-20 19:23:21 -0600

    services worked offline v1.2

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,8 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
 };
 
 export default withPWA({
@@ -17,4 +13,39 @@ export default withPWA({
   disable: !isProd,
   register: true,
   skipWaiting: true,
+  runtimeCaching: [
+    {
+      urlPattern: /^https?.*/, // Cacheo de páginas y assets
+      handler: "NetworkFirst",
+      options: {
+        cacheName: "pages-cache",
+        networkTimeoutSeconds: 3,
+        expiration: {
+          maxEntries: 50,
+          maxAgeSeconds: 60 * 60 * 24 * 30,
+        },
+        cacheableResponse: {
+          statuses: [0, 200],
+        },
+      },
+    },
+    {
+      urlPattern: /\/api\/.*$/, // API
+      handler: "NetworkFirst",
+      options: {
+        cacheName: "api-cache",
+      },
+    },
+    {
+      urlPattern: /\.(png|jpg|jpeg|svg|gif|webp)$/,
+      handler: "CacheFirst",
+      options: {
+        cacheName: "images-cache",
+        expiration: {
+          maxEntries: 200,
+          maxAgeSeconds: 60 * 60 * 24 * 30,
+        },
+      },
+    },
+  ],
 })(nextConfig);
